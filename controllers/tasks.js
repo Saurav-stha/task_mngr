e9cb5b6b02ab42999600159c953401e64fde0af2
saurav <shresthsaurav8@gmail.com>
2024-07-05 20:04:32 +0545

    feat: add CRUD

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,6 +1,12 @@
 const Task = require("../models/Task")
 
-const getAllTasks = (req, res) => {
+const getAllTasks = async (req, res) => {
+    try{
+        const tasks = await Task.find({})
+        res.status(200).json({ tasks })
+    }catch(err){
+        res.status(500).json({err})
+    }
     res.send("get sabai taskss")
 }
 const createTask = async(req, res) => {
@@ -15,14 +21,59 @@ const createTask = async(req, res) => {
     // res.json(req.body)
 }
 
-const getTask = (req, res) => {
-    res.json({tid:req.params.tid})
+const getTask = async (req, res) => {
+    try{
+        const {tid:taskID} = req.params
+        const task = await Task.findOne({ _id:taskID });
+
+        if (!task){
+            // correct syntax of id but no such id in db
+            return res.status(404).json({ msg: `task with id : ${taskID} xaina houu` })
+        }
+
+        res.status(200).json({ task })
+    }
+    catch(err){
+        // wrong syntax of id
+        res.status(500).json({msg: err})
+
+    }
+    // res.json({ tid:req.params.tid })
 }
-const deleteTask = (req, res) => {
-    res.send("del euta task")
+const deleteTask = async (req, res) => {
+    try{
+        const {tid:taskId} = req.params
+        const task = await Task.findByIdAndDelete({_id:taskId})
+
+        if (!task){
+            return res.status(404).json({ msg: `Testo ${taskId} vako task xaina houu` })
+        }
+
+        res.status(200).json({ task })
+    }
+    catch(err){
+        res.status(500).json({ err })
+    }
+    // res.send("del euta task")
 }
-const updateTask = (req, res) => {
-    res.send("update euta task")
+const updateTask = async (req, res) => {
+    try{
+        const {tid:taskId} = req.params
+
+        const task = await Task.findByIdAndUpdate({ _id: taskId}, req.body,{
+            new:true,
+            runValidators:true
+        } )
+        
+        if (!task){
+            return res.status(404).json({ msg: `Testo ${taskId} id vako task xaina yoi` })
+        }
+        
+        res.status(200).json({ task })
+    }catch(e){
+        res.status(500).json({e})
+    }
+    // res.send("update euta task")
 }
 module.exports = {
     getAllTasks,
